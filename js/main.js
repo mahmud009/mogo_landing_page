cc59a1d9c24897270334be4f753ac7763500098c
mahmud009 <mahmud.est@gmail.com>
2020-03-22 23:23:52 +0600

    #added quotes and testimoni section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $(function() {
+  //Initialize counterjs
   $(".counter").counterUp({
     delay: 10,
     time: 1500
+  });
+
+  // Slick js initialize
+  $("#quotes-1 .qts-wrap").slick({
+    prevArrow: $(".slick-cs-prev"),
+    nextArrow: $(".slick-cs-next")
+  });
+
+  $("#quotes-2 .testimoni-wrap").slick({
+    prevArrow: $(".slick-cs2-prev"),
+    nextArrow: $(".slick-cs2-next")
   });
 });
