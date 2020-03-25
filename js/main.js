77dea34b12f90c76b08e7f319b99cd5e02b4ae95
mahmud009 <mahmud.est@gmail.com>
2020-03-26 04:26:25 +0600

    added slider on banner

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $(function() {
   //header scroll class
-
   $(window).on("scroll", function(e) {
     if ($(window).scrollTop() > 200) {
       $("header").addClass("header-scrolled");
@@ -9,13 +8,46 @@ $(function() {
     }
   });
 
+  // Banner section Slick js initialize
+  let customDots = $(".bnr-slide-indic-wrp").children();
+  let slideDuration = 5000;
+
+  //pauseOnFocus
+  //pauseOnHover
+
+  $(".intro-cnt-wrap").on("init afterChange", function(e, s) {
+    $(".bnr-slide-indic").removeClass("bnr-slide-indic-actv");
+    $(customDots[s.currentSlide]).addClass("bnr-slide-indic-actv");
+
+    let activeIndic = document.querySelector(
+      ".bnr-slide-indic-actv .ldr-thumb"
+    );
+
+    activeIndic.style.width = "100%";
+    activeIndic.style.transition = `transform ${slideDuration / 1000}s`;
+    activeIndic.style.transitionTimingFunction = "linear";
+  });
+
+  const slick = $(".intro-cnt-wrap").slick({
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    autoplay: true,
+    autoplaySpeed: slideDuration,
+    pauseOnFocus: false,
+    pauseOnHover: false
+    // dots: true,
+    // appendDots: $(".bnr-slide-indic")
+  });
+
+  console.log();
   //Initialize counterjs
   $(".counter").counterUp({
     delay: 10,
     time: 1500
   });
 
-  // Slick js initialize
+  // Quotes and testimoni section Slick js initialize
   $("#quotes-1 .qts-wrap").slick({
     prevArrow: $(".slick-cs-prev"),
     nextArrow: $(".slick-cs-next")
@@ -26,5 +58,8 @@ $(function() {
     nextArrow: $(".slick-cs2-next")
   });
 
-  // Custom collapse
+  // Jquery UI accordion initialize
+  $("#accordion1").accordion({
+    icons: null
+  });
 });
